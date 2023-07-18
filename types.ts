4b3f88c46f610f3c304e5a4f42d746a151bec94b
William M. Tsikata <tsikatawill@gmail.com>
2023-07-18 20:14:02 +0000

    add testimonial x service types

---
+++ types.ts
@@ -0,0 +1,12 @@
+export type Service = {
+  description: string;
+  icon: React.ReactNode;
+  title: string;
+};
+
+export type Testimonial = {
+  image: string;
+  name: string;
+  position: string;
+  text: string;
+};
