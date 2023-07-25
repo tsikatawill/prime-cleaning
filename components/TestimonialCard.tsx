9eb06bfe8c3cdf0ac3669de3751e0351c01c9b33
William M. Tsikata <tsikatawill@gmail.com>
2023-07-25 16:20:27 +0000

    create testimonal card

--- components/TestimonialCard.tsx
+++ components/TestimonialCard.tsx
@@ -3,21 +3,21 @@ import Image from "next/image";
 
 const TestimonialCard = ({ image, name, position, text }: Testimonial) => {
   return (
-    <div className="card bg-tertiary sm:max-w-xs sm:flex-shrink-0 p-5 rounded-lg text-white">
+    <div className="card bg-tertiary bg-opacity-30 hover:border-tertiary transition-all duration-200 ease-out border border-transparent max-w-[18rem] sm:max-w-xs flex-shrink-0 p-3 md:p-5 rounded-lg text-white">
       <div className="flex flex-col items-center">
         <div className="h-12 w-12 rounded-full bg-slate-200 mb-3 overflow-hidden">
           <Image src={image} width={100} height={100} alt={name} />
         </div>
 
         <div className="mb-4 text-center">
-          <span className="block font-bold text-lg leading-4 m-0 p-0">
+          <span className="block font-bold sm:text-lg leading-[4px]">
             {name}
           </span>
           <small className="text-slate-100">{position}</small>
         </div>
       </div>
 
-      <p className="text-center">{text}</p>
+      <p className="text-center text-sm sm:text-base">{text}</p>
     </div>
   );
 };
