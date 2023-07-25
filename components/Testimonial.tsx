f319d444f4b689bf7f5fcbab988c6c205d7d705d
William M. Tsikata <tsikatawill@gmail.com>
2023-07-25 16:20:15 +0000

    prettify code

--- components/Testimonial.tsx
+++ components/Testimonial.tsx
@@ -1,21 +1,21 @@
-import Image from "next/image";
 import Container from "./Container";
 import TestimonialCard from "./TestimonialCard";
 import { TESTIMONIALS } from "@/lib/testimonials";
+import { motion } from "framer-motion";
 
 const Testimonial = () => {
   return (
-    <section>
-      <Container className="pt-5 sm:pt-10">
+    <section className="bg-primary text-white" id="testimonial">
+      <Container className="py-5 sm:py-10">
         <h2 className="font-bold text-3xl text-center">
-          What <span className="text-primary">our customers say</span>
+          What <span className="text-accent">our customers</span> say
         </h2>
 
-        <div className="flex gap-5 overflow-x-scroll mt-5">
+        <motion.div className="flex gap-5 overflow-x-scroll mt-5 custom-scrollbar scroll-x pb-5">
           {TESTIMONIALS.map((item, idx) => (
             <TestimonialCard key={idx} {...item} />
           ))}
-        </div>
+        </motion.div>
       </Container>
     </section>
   );
